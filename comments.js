12fea1f3aff5fe062672302a31a197a3aa0152ea
Yorlan Males-Araujo <whyyorlantaken@gmail.com>
2024-12-24 20:17:35 +0000

    Testing

--- comments.js
+++ comments.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Create a web service
-
+// Create web service
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
